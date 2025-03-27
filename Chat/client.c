0a4fd826a61ea6f082b4e9c814016a238ccbf087
AakashJai06 <127650255+AakashJai06@users.noreply.github.com>
2025-03-27 08:45:01 +0530

    Create client.c

---
+++ Chat/client.c
@@ -0,0 +1,59 @@
+/*
+ * TCP Chat Client
+ * Connects to the server and allows message exchange.
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+#define PORT 8080
+#define BUFFER_SIZE 1024
+
+int main() {
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+
+    // Create client socket
+    client_socket = socket(AF_INET, SOCK_STREAM, 0);
+    if (client_socket == -1) {
+        perror("Socket creation failed");
+        exit(EXIT_FAILURE);
+    }
+
+    // Configure server address
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(PORT);
+    server_addr.sin_addr.s_addr = inet_addr("127.0.0.1");  // Localhost
+
+    // Connect to the server
+    if (connect(client_socket, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("Connection failed");
+        close(client_socket);
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Connected to server!\n");
+
+    while (1) {
+        // Send message to server
+        printf("Client: ");
+        fgets(buffer, BUFFER_SIZE, stdin);
+        send(client_socket, buffer, strlen(buffer), 0);
+
+        // Receive response from server
+        memset(buffer, 0, BUFFER_SIZE);
+        int bytes_received = recv(client_socket, buffer, BUFFER_SIZE, 0);
+        if (bytes_received <= 0) {
+            printf("Server disconnected.\n");
+            break;
+        }
+        printf("Server: %s", buffer);
+    }
+
+    close(client_socket);
+    return 0;
+}
