14f752b6d2dcbd06495602306126db21a5ef1725
Aakash J John <127650255+AakashJai06@users.noreply.github.com>
2025-04-08 17:29:06 +0530

    Update clientTCP.c

--- tcp/clientTCP.c
+++ tcp/clientTCP.c
@@ -1,50 +1,31 @@
 #include <sys/socket.h>
-#include <netinet/in.h>
 #include <stdio.h>
-#include <string.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <arpa/inet.h>  
+#include <string.h>
+#include <arpa/inet.h>
 
-int main() {
-    char buffer[100];
-    int k; // Error Flag
-    int sock_desc;
-    struct sockaddr_in server;
+#define BUFFER_SIZE 1024
+#define PORT 8080
 
-    /* Creating Socket */
-    sock_desc = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock_desc == -1) {
-        printf("Error In Socket Creation!\n");
-        return 1;
-    }
-    printf("Socket Created Successfully!\n");
+int main(){
+  int sockfd,newsock;
+  struct sockaddr_in server;
+  socklen_t len = sizeof(server);
+  char buffer[BUFFER_SIZE];
 
-    server.sin_family = AF_INET;
-    server.sin_port = htons(3003);  
-    server.sin_addr.s_addr = inet_addr("127.0.0.1");  
+  sockfd = socket(AF_INET,SOCK_STREAM,0);
 
-    /* Connecting Process */
-    k = connect(sock_desc, (struct sockaddr *)&server, sizeof(server));
-    if (k == -1) {
-        printf("Error In Connecting To The Server!\n");
-        return 1;
-    }
-    printf("Connection Successful!\n");
+  server.sin_family = AF_INET;
+  server.sin_port = htons(PORT);
+  server.sin_addr.s_addr = INADDR_ANY;
 
-    /* Data Sending Process */
-    printf("\nEnter The Data To Be Sent: ");
-    fgets(buffer, 100, stdin);
-    buffer[strcspn(buffer, "\n")] = '\0'; 
+  connect(sockfd,(struct sockaddr *) &server,sizeof(server));
 
-    k = send(sock_desc, buffer, strlen(buffer), 0);
-    if (k == -1) {
-        printf("Error In Sending Data!\n");
-        return 1;
-    }
-    printf("Message Sent!\n");
+  printf("Enter Message to send : ");
+  fgets(buffer,100,stdin);
+  send(sockfd,buffer,strlen(buffer),0);
 
-    close(sock_desc);
-
-    return 0;
+    close(sockfd);
+  return 0;
 }
